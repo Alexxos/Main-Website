b7af07d55ddbbb3dc4ab4b68f39470f43817e9c2
Alexxos <alexos.droid@gmail.com>
2020-05-03 16:19:52 +0700

    Traversy's stuff

--- scripts/index.js
+++ scripts/index.js
@@ -1,25 +1,27 @@
 class App extends React.Component {
-    render() {
-        return (
-         <div>   
-            <Machine
-            s1="🎃" 
-            s2="🍕"
-            s3 = "☠"
-            />
-            <Machine
-            s1="🎃" 
-            s2="🍕"
-            s3 = "☠"
-            />
-            <Machine
-            s1="🎃" 
-            s2="🍕"
-            s3 = "☠"
-            />
-          </div>
-        )    
-    } 
+  render() {
+    return (
+      <div>
+        <Machine
+          s1="🎃"
+          s2="🍕"
+          s3="☠"
+        />
+        <Machine
+          s1="🎃"
+          s2="🍕"
+          s3="☠"
+        />
+        <Machine
+          s1="🎃"
+          s2="🍕"
+          s3="☠"
+        />
+      </div>
+    )
+  }
 }
 
+
+const arr = [1, 2, 3, 4, 5]
 ReactDOM.render(<App />, document.getElementById('root'));
